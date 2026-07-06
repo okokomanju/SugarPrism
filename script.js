4b0d36a98f1d07315bee7e543e2bacbf4140ea6a
okoko <167289410+okokomanju@users.noreply.github.com>
2026-07-07 00:32:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// 星を生成
+const stars = document.querySelector('.stars');
+
+for(let i = 0; i < 120; i++){
+
+  const star = document.createElement('span');
+  star.className = 'star';
+
+  const size = Math.random() * 4 + 1;
+
+  star.style.width = size + 'px';
+  star.style.height = size + 'px';
+
+  star.style.left = Math.random() * 100 + 'vw';
+  star.style.top = Math.random() * 100 + 'vh';
+
+  star.style.animationDuration = (2 + Math.random() * 5) + 's';
+  star.style.animationDelay = (Math.random() * 5) + 's';
+
+  stars.appendChild(star);
+}
