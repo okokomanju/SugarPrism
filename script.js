32eb48ded251b0cfa609f3577dfd8e94656c9e40
okoko <167289410+okokomanju@users.noreply.github.com>
2026-07-09 12:25:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -31,3 +31,22 @@ function floatNews() {
 }
 
 floatNews();
+
+const audio = document.getElementById("preview");
+const btn = document.getElementById("playBtn");
+
+btn.addEventListener("click", () => {
+
+    if(audio.paused){
+
+        audio.play();
+        btn.textContent="❚❚ PAUSE";
+
+    }else{
+
+        audio.pause();
+        btn.textContent="▶︎ PLAY";
+
+    }
+
+});
